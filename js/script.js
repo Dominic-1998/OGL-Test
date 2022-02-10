c0be9d8c68908031274d3af63c3b57966ec7cf42
Dominic Byrne <dominicbyrne12@gmail.com>
2022-02-10 11:52:27 +0000

    fetch working into html table

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 getProducts().catch(error =>{
   console.log(error);
-})
+});
 async function getProducts(){
   let response = await fetch('https://testapi.oglsoftware.co.uk/products');
   let data = await response.json();
-  console.log(data);
+  let tbody = document.getElementById('tbody');
   for (const key in data['data']){
-    console.log(data['data'][key]['sku'])
+    let row = tbody.insertRow(key);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
+    cell1.innerHTML = data['data'][key]['sku'];
+    cell2.innerHTML = data['data'][key]['name'];
+    cell3.innerHTML = data['data'][key]['description'];
+    cell4.innerHTML = data['data'][key]['stock-level'];
+    cell5.innerHTML = data['data'][key]['price'];
+    // console.log(data['data'][key]['sku']);
+    // console.log(data['data'][key]['name']);
+    // console.log(data['data'][key]['description']);
+    // console.log(data['data'][key]['stock-level']);
+    // console.log(data['data'][key]['price']);
+    // console.log(data['data'][key]['image']);
   }
-}
+};
 
 // await fetch('https://testapi.oglsoftware.co.uk/products')
 //   .then(response => {
